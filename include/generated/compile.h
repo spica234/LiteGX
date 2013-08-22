a7237132e279e360ec34f95ce1b45c14423b7fea
root <root@amidala.cyxmachines.org>
2013-08-22 21:25:50 +0200

    Initial commit LiteGX v1.4

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 37 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#37 SMP PREEMPT Mon Aug 19 14:07:56 CEST 2013"
+#define LINUX_COMPILE_BY "androidgx"
+#define LINUX_COMPILE_HOST "kernel"
+#define LINUX_COMPILER "gcc version 4.7.4 20130626 (prerelease) (Linaro GCC 4.7-2013.07) "

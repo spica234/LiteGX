2f83a72ddceed2479c12815b7c0277ccd24c29f5
AndroidGX <subut3x@gmail.com>
2013-08-28 20:55:12 +0200

    v2.3 : (check changelog)

--- Makefile
+++ Makefile
@@ -350,7 +350,7 @@ CHECKFLAGS     := -D__linux__ -Dlinux -D__STDC__ -Dunix -D__unix__ \
 CFLAGS_MODULE   =
 AFLAGS_MODULE   =
 LDFLAGS_MODULE  = --strip-debug
-CFLAGS_KERNEL	=
+CFLAGS_KERNEL	= -funswitch-loops -fpredictive-commoning -fgcse-after-reload -ftree-vectorize -fipa-cp-clone -fsingle-precision-constant -pipe
 AFLAGS_KERNEL	=
 CFLAGS_GCOV	= -fprofile-arcs -ftest-coverage
 
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 19 */
+/* This file is auto generated, version 22 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#19 SMP PREEMPT Tue Aug 27 23:15:42 CEST 2013"
+#define UTS_VERSION "#22 SMP PREEMPT Wed Aug 28 20:47:51 CEST 2013"
 #define LINUX_COMPILE_BY "androidgx"
 #define LINUX_COMPILE_HOST "kernel"
 #define LINUX_COMPILER "gcc version 4.7.4 20130626 (prerelease) (Linaro GCC 4.7-2013.07) "

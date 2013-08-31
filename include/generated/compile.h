0505544fd78de6b3b169e3798521876fa73d2101
AndroidGX <subut3x@gmail.com>
2013-08-31 12:43:39 +0200

    v2.6 : (check changelog)

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 29 */
+/* This file is auto generated, version 30 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#29 SMP PREEMPT Fri Aug 30 14:32:42 CEST 2013"
+#define UTS_VERSION "#30 SMP PREEMPT Sat Aug 31 10:09:13 CEST 2013"
 #define LINUX_COMPILE_BY "androidgx"
 #define LINUX_COMPILE_HOST "kernel"
 #define LINUX_COMPILER "gcc version 4.7.4 20130626 (prerelease) (Linaro GCC 4.7-2013.07) "

9f4f98548cd9d0495b1f4c86875bc223de804ff8
AndroidGX <subut3x@gmail.com>
2013-09-06 10:45:20 +0200

    v3.2 : check changelog

--- drivers/battery/samsung_battery.c
+++ drivers/battery/samsung_battery.c
@@ -1726,7 +1726,7 @@ monitor_finish:
 		info->prev_charge_virt_state != info->charge_virt_state ||
 		info->prev_battery_soc != info->battery_soc) {
 		/* TBD : timeout value */
-		pr_info("%s: update wakelock(%d)\n", __func__, HZ);
+		pr_info("%s : update wakelock (%d)\n", __func__, HZ);
 		wake_lock_timeout(&info->update_wake_lock, HZ);
 	}
 

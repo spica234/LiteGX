0e5e5f5f0871f864691bbba7764ac63a6b939d22
AndroidGX <subut3x@gmail.com>
2013-09-18 19:49:36 +0200

    v4.3.0 : Chech changelog

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Wed Sep 18 12:49:38 CEST 2013"
+#define UTS_VERSION "#6 SMP PREEMPT Wed Sep 18 19:07:58 CEST 2013"
 #define LINUX_COMPILE_BY "androidgx"
 #define LINUX_COMPILE_HOST "kernel"
 #define LINUX_COMPILER "gcc version 4.7.4 20130812 (prerelease) (Linaro GCC 4.7-2013.08) "
